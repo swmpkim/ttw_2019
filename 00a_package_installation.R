7f64953a36a579b632d4f8fb74d2f19d2897960f
swmpkim <kimberly.cressman@dmr.ms.gov>
2019-02-20 09:35:49 -0600

    initial commit - pkg installation and check files

---
+++ 00a_package_installation.R
@@ -0,0 +1,24 @@
+# packages to install for SWMP Status Report workflow
+# modified by Kim Cressman from status report script 00_initial_installation.R
+
+
+pkg_data_wrangle <- c('broom', 'dplyr', 'lubridate', 'magrittr', 'tidyr', 'rlang', 'readxl')
+pkg_analysis <- c('EnvStats', 'ggplot2', 'ggthemes', 'scales', 'RColorBrewer', 'stringr')
+pkg_map <- c('leaflet', 'maptools', 'rgdal', 'rgeos', 'sp', 'mapview', 'webshot')
+pkg_reporting <- c('flextable', 'officer')
+pkg_swmp <- c('SWMPr', 'SWMPrExtension', 'devtools')
+
+
+install.packages(c(pkg_data_wrangle, pkg_analysis, pkg_map, pkg_reporting, pkg_swmp))
+
+
+# Load webshot package and install phantomjs
+## phantomjs is required to make the grayscale reserve level maps
+library(webshot)
+install_phantomjs()
+ 
+# Install/load "installr" package:
+#if(!require(installr)) { install.packages("installr", repos = "http://cran.us.r-project.org"); require(installr)} #load / install+load installr
+
+# Install pandoc:
+#install.pandoc()
---
+++ 00b_package_check.R
@@ -0,0 +1,44 @@
+################ INSTRUCTIONS ##################################
+### Use Ctrl+A (Windows) / Cmd+A (Mac)   to select this entire script
+### Then Ctrl+Enter / Cmd+Enter  to run it
+### Then check the output in your console and see if you 
+### need to re-install any packages
+
+### If you do, I recommend trying one at a time, using the command
+### install.packages("package_name_here")
+
+### Then re-run this script to make sure everything worked
+
+### If you have any problems, contact Kim
+
+################################################################
+
+# list of packages we need to have installed for our workflow
+# original vectors copied out of status report script 00_initial_installation.R
+
+pkg_data_wrangle <- c('broom', 'dplyr', 'lubridate', 'magrittr', 'tidyr', 'rlang', 'readxl')
+pkg_analysis <- c('EnvStats', 'ggplot2', 'ggthemes', 'scales', 'RColorBrewer', 'stringr')
+pkg_map <- c('leaflet', 'maptools', 'rgdal', 'rgeos', 'sp', 'mapview', 'webshot')
+pkg_reporting <- c('flextable', 'officer')
+pkg_swmp <- c('SWMPr', 'SWMPrExtension', 'devtools')
+
+
+# glue the sub-categories together into one vector of packages we need
+pkgs_needed <- c(pkg_data_wrangle, pkg_analysis, pkg_map, pkg_reporting, pkg_swmp)
+
+
+# use installed.packages() to find out if the ones we want are installed
+# this returns a giant data frame;
+# we want all rows, but only the first column
+pkgs_installed <- installed.packages()[ , 1]
+
+
+# make and print a vector of packages on our "pkgs_needed" list that
+# are *not* in the list of pkgs_installed
+pkgs_missing_index <- which(!(pkgs_needed %in% pkgs_installed))
+pkgs_missing <- pkgs_needed[pkgs_missing_index]
+
+
+if(length(pkgs_missing) == 0){
+        print("All required packages are installed!")
+} else{ print("You need to install the following packages:"); cat(pkgs_missing, sep="\n") }
