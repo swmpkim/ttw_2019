cb8147b7c04fc12752679e8e6d91b651db488949
swmpkim <kimberly.cressman@dmr.ms.gov>
2019-03-01 09:17:14 -0600

    rewritten to actually load packages and report if they fail

--- 00b_package_check.R
+++ 00b_package_check.R
@@ -24,25 +24,29 @@ pkg_swmp <- c('SWMPr', 'SWMPrExtension', 'devtools')
 pkg_ttw_training <- c('rmarkdown', 'knitr', 'ggThemeAssist', 'clifro')
 
 
-
 # glue the sub-categories together into one vector of packages we need
 pkgs_needed <- c(pkg_data_wrangle, pkg_analysis, pkg_map, 
                  pkg_reporting, pkg_swmp, pkg_ttw_training)
 
 
-# use installed.packages() to find out if the ones we want are installed
-# this returns a giant data frame;
-# we want all rows, but only the first column
-pkgs_installed <- installed.packages()[ , 1]
+# now try loading each one and see if it works
+
+# set up an output vector
+pkg_result <- vector("logical", length(pkgs_needed))
+
+# loop through the needed packages
+for(i in seq_along(pkgs_needed)){
+        # try to load the package and report whether it works
+        # record that TRUE or FALSE in the pkg_result vector
+        pkg_result[i] <- library(pkgs_needed[i], character.only = TRUE, quietly = TRUE, logical.return = TRUE)
+
+}
+
+# make the vector of missing packages:
+# pkgs_needed that failed to load
+pkgs_missing <- pkgs_needed[!pkg_result]
 
 
-# make and print a vector of packages on our "pkgs_needed" list that
-# are *not* in the list of pkgs_installed
-pkgs_missing_index <- which(!(pkgs_needed %in% pkgs_installed))
-pkgs_missing <- pkgs_needed[pkgs_missing_index]
-
-
-# print the results of this check to the console
 if(length(pkgs_missing) == 0){
-        message("All required packages are installed!")
-} else{ message("You need to install the following packages: "); cat(pkgs_missing, sep="\n") }
+        message("\n \nAll required packages are installed and loading properly! \n \n")
+} else{ message("\n \nYou need to install the following packages: "); cat(pkgs_missing, sep="\n") }
